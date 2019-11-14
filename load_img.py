ad78882478037cdbd7449ba8bfd1df2acfe75f5d
medicharlachiranjeevi <medicharlachiranjeevi@gmail.com>
2019-11-14 15:36:20 +0530

    Create load_img.py

---
+++ load_img.py
@@ -0,0 +1,16 @@
+import numpy as np
+from tensorflow.python.keras.applications.resnet50 import preprocess_input
+from tensorflow.python.keras.preprocessing.image import load_img, img_to_array
+
+image_size = 224
+
+def read_and_prep_images(img_paths, img_height=image_size, img_width=image_size):
+    imgs = [load_img(img_path, target_size=(img_height, img_width)) for img_path in img_paths]
+    img_array = np.array([img_to_array(img) for img in imgs])
+    output = preprocess_input(img_array)
+    return(output)
+from tensorflow.python.keras.applications import ResNet50
+
+my_model = ResNet50(weights='../input/resnet50/resnet50_weights_tf_dim_ordering_tf_kernels.h5')
+test_data = read_and_prep_images(img_paths)
+preds = my_model.predict(test_data) 
